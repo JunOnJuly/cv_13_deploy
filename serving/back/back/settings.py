310150b2ca5022181108df2f148aabdb6fc21706
JunOnJuly <wlgnstjd0717@gmail.com>
2023-07-06 01:32:34 +0900

    fix whitelist

--- serving/back/back/settings.py
+++ serving/back/back/settings.py
@@ -134,5 +134,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ORIGIN_WHITELIST = [
     "http://34.64.255.206:3000",
-    "http://172.17.0.3:3000"
+    "http://172.17.0.3:3000",
+    'http://localhost:3000'
 ]
+
+CORS_ALLOW_CREDENTIALS = True
